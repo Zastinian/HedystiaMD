3bd6871b34a65918e0fcbba8dd6502627bf75312
Zastinian <zastinian@gmail.com>
2023-03-25 15:14:00 -0600

    feat: index

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ async function startEsmile() {
         displayName: await esmile.getName(i + "@s.whatsapp.net"),
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await esmile.getName(i + "@s.whatsapp.net")}\nFN:${await esmile.getName(
           i + "@s.whatsapp.net"
-        )}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:okeae2410@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://instagram.com/cak_haho\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
+        )}\nitem1.TEL;waid=${i}:${i}\nitem1.X-ABLabel:Ponsel\nitem2.EMAIL;type=INTERNET:contact@mresmile.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/zastinianyt\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Indonesia;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
       });
     }
     esmile.sendMessage(jid, {contacts: {displayName: `${list.length} Kontak`, contacts: list}, ...opts}, {quoted});
