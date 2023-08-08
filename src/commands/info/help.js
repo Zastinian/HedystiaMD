bab2e71123387b58f19bdaec8025d96cbdc47c80
Zastinian <82119938+Zastinian@users.noreply.github.com>
2023-08-07 18:57:52 -0600

    Update help.js

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -2,23 +2,6 @@ module.exports = {
   name: "help",
   run: async (bot, message, global, args, text) => {
     const txt = `*┏━━━━━━━━━ツ━━━━━━━━━┓*\n  _*⤝  Categories ⤞*_\n  ⟿ ${global.prefix}Information\n  ⟿ ${global.prefix}Interaction\n  ⟿ ${global.prefix}Music\n*┗━━━━━━━━━ツ━━━━━━━━━┛*`;
-    let btn = [
-      {
-        urlButton: {
-          displayText: "Website",
-          url: "https://hedystia.com",
-        },
-      },
-      //Temporary disable due to whatsapp update
-      /*{
-        quickReplyButton: {
-          displayText: "",
-          id: `${global.prefix}`,
-        },
-      },*/
-    ];
     bot.sendMessage(message.chat, {text: txt}, {quoted: message});
-    //bot.sendButtonText(message.chat, btn, txt, "Hedystia", message);
-    //bot.send5ButImg(message.chat, txt, bot.user.name, global.thumb, btn);
   },
 };
