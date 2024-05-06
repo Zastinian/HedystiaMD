3a7eb87e087e860ab2aaae52d936ea87b39a2fc5
Zastinian <zastinian@gmail.com>
2024-05-06 17:36:07 -0600

    fix: Added admin perm to hidetag

--- src/commands/utils/hidetag.js
+++ src/commands/utils/hidetag.js
@@ -1,8 +1,15 @@
 module.exports = {
   name: "hidetag",
-  run: async ({ bot, botNumber, text, message, group }) => {
-    const participants = await group.participants;
-    const users = participants.filter((v) => v.id !== botNumber).map((u) => bot.decodeJid(u.id));
-    bot.sendMessage(message.chat, { text: `${text.length <= 0 ? "Hi!" : text}`, mentions: users });
+  run: async ({ bot, botNumber, lang, text, message, group, types }) => {
+    if (types.isGroupOwners || types.isGroupAdmins) {
+      const participants = await group.participants;
+      const users = participants.filter((v) => v.id !== botNumber).map((u) => bot.decodeJid(u.id));
+      bot.sendMessage(message.chat, {
+        text: `${text.length <= 0 ? "Hi!" : text}`,
+        mentions: users,
+      });
+    } else {
+      return bot.sendMessage(message.chat, { text: lang.global.noUserAdmin }, { quoted: message });
+    }
   },
 };
