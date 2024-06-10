2e21e338f024aa836d73af368b5ec3acfca60466
Zastinian <zastinian@gmail.com>
2024-06-09 19:28:19 -0600

    fix: Infinity Init

--- src/commands/owner/init.js
+++ src/commands/owner/init.js
@@ -1,6 +1,9 @@
 module.exports = {
   name: "init",
   run: async ({ bot, lang, message }) => {
+    if (global.db.config.select("owner", { id: "owner" })[0].value) {
+      return bot.sendMessage(message.chat, { text: lang.owner.alreadyInit }, { quoted: message });
+    }
     global.db.config.update("owner", { id: "owner" }, { value: message.sender });
     return bot.sendMessage(message.chat, { text: lang.owner.defaultOwner }, { quoted: message });
   },
