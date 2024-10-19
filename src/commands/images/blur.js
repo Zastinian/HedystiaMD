4eca4d18b97494cc56c889c42e3baf5851b9a873
Zastinian <zastinian@gmail.com>
2024-10-19 11:34:23 -0600

    feat: Change url

--- src/commands/images/blur.js
+++ src/commands/images/blur.js
@@ -14,7 +14,7 @@ module.exports = {
           ? bot.user.jid
           : message.sender;
     const response = await fetch(
-      `https://strangeapi.hostz.me/api/filters/blur?image=${encodeURIComponent(
+      `https://gens.hedystia.com/filters/blur?image=${encodeURIComponent(
         await bot
           .profilePictureUrl(user)
           .catch(
@@ -24,7 +24,7 @@ module.exports = {
       )}}`,
       {
         headers: {
-          Authorization: `Bearer ${global.imageToken}`,
+          Authorization: "Hedystia",
         },
       },
     );
--- src/commands/images/wasted.js
+++ src/commands/images/wasted.js
@@ -14,7 +14,7 @@ module.exports = {
           ? bot.user.jid
           : message.sender;
     const response = await fetch(
-      `https://strangeapi.hostz.me/api/overlays/wasted?image=${encodeURIComponent(
+      `https://gens.hedystia.com/generators/wasted?image=${encodeURIComponent(
         await bot
           .profilePictureUrl(user)
           .catch(
@@ -24,7 +24,7 @@ module.exports = {
       )}}`,
       {
         headers: {
-          Authorization: `Bearer ${global.imageToken}`,
+          Authorization: "Hedystia",
         },
       },
     );
